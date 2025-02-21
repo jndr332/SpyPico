c9b7ecd1f5aa7793607564551b96c4833032f9ee
Juan Saenz MITM <80866984+jndr332@users.noreply.github.com>
2025-02-21 17:45:24 -0500

    Update settings.py

--- SpyPico/SpyPico/settings.py
+++ SpyPico/SpyPico/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'victimas',
         'USER':'userpico',
-        'PASSWORD':'DosDos..',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'5432',
     }
